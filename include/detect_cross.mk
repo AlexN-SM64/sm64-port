112bf44b57b2224a05f8a2f5b2ae67eb20a9e122
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-12-09 22:58:57 +0300

    Update something

--- include/detect_cross.mk
+++ include/detect_cross.mk
@@ -37,70 +37,15 @@ endif
 
 ARCHITECTURE ?= $(shell arch)
 
-PLATFORM_CROSS := $(ARCHITECTURE)-w64-mingw32-
-
-else
-
-AARCH64_CROSS := $(shell bash print_cmd_cross.sh aarch64)
-X86_64_CROSS := $(shell bash print_cmd_cross.sh x86_64)
-I686_CROSS := $(shell bash print_cmd_cross.sh i686)
-
-endif
-
-# Detect architectures for Linux builds
-ifeq ($(TARGET_LINUX),1)
-
-ifeq ($(shell arch),aarch64)
-
-  ifneq ($(call find-command,$(AARCH64_CROSS)gcc),)
-    ARCHITECTURE ?= aarch64
-  else ifneq ($(call find-command,$(X86_64_CROSS)gcc),)
-    ARCHITECTURE ?= x86_64
-  else ifneq ($(call find-command,$(I686_CROSS)gcc),)
-    ARCHITECTURE ?= i686
-  else
-    ARCHITECTURE ?= aarch64
-  endif
-  
-  $(eval $(call validate-option,ARCHITECTURE,aarch64 x86_64 i686))
-
-else ifeq ($(shell arch),x86_64)
-
-  ifneq ($(call find-command,$(X86_64_CROSS)gcc),)
-    ARCHITECTURE ?= x86_64
-  else ifneq ($(call find-command,$(I686_CROSS)gcc),)
-    ARCHITECTURE ?= i686
-  else
-    ARCHITECTURE ?= x86_64
-  endif
-  
-  $(eval $(call validate-option,ARCHITECTURE,x86_64 i686))
-
-else ifeq ($(shell arch),i686)
-
-  ARCHITECTURE ?= i686
-  
-  $(eval $(call validate-option,ARCHITECTURE,i686))
-
-endif
-
-ARCHITECTURE ?= $(shell arch)
-
-PLATFORM_CROSS := $(shell bash print_cmd_cross.sh $(ARCHITECTURE))
+PLATFORM_CROSS ?= $(ARCHITECTURE)-w64-mingw32-
 
 endif
 
 ifeq ($(TARGET_N64),1)
 
 # Detect MIPS prefix for N64 builds
-MIPS_ARCH_CROSS := $(shell bash print_cmd_cross.sh mips)
-MIPS64_ARCH_CROSS := $(shell bash print_cmd_cross.sh mips64)
-
-ifneq ($(call find-command,$(MIPS_ARCH_CROSS)ld),)
-  MIPS_CROSS := $(MIPS_ARCH_CROSS)
-else ifneq ($(call find-command,$(MIPS64_ARCH_CROSS)ld),)
-  MIPS_CROSS := $(MIPS64_ARCH_CROSS)
-else
+MIPS_CROSS := $(shell bash print_cmd_cross.sh mips)
+ifeq ($(call find-command,$(shell bash print_cmd_cross.sh mips)ld),)
   $(error Unable to detect a suitable MIPS toolchain installed)
 endif
 
@@ -109,12 +54,10 @@ ifneq ($(shell arch),x86_64)
 ifneq ($(shell arch),i686)
 ifeq ($(NO_CHECK),0)
 
-  ifneq ($(call find-command,$(X86_64_CROSS)gcc),)
-    CC_CHECK_CROSS := $(X86_64_CROSS)
-  else ifneq ($(call find-command,$(I686_CROSS)gcc),)
-    CC_CHECK_CROSS := $(I686_CROSS)
-  else
-    $(error CC check for N64 build is only possible for GCC i686 or x86_64 architectures. Make sure NO_CHECK is set to 1)
+  ifneq ($(call find-command,x86_64-linux-gnu-gcc),)
+    CC_CHECK_CROSS := x86_64-linux-gnu-
+  else ifneq ($(call find-command,i686-linux-gnu-gcc),)
+    CC_CHECK_CROSS := i686-linux-gnu-
   endif
 
 endif
--- print_cmd_cross.sh
+++ print_cmd_cross.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $(for DS in $(compgen -c $1); do echo "${DS%-*}"; done | tail -1)-$2
+echo $(for DS in $(compgen -c $1 | sort); do echo "${DS%-*}"; done | tail -1)-$2
